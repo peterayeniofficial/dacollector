ffa735173d01783b15ef7367a2484d3d21abd859
Peter Ayeni <ayenicology@gmail.com>
2019-11-25 09:50:37 +0000

    added migration

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,4 @@
 class Collection < ApplicationRecord
+  belongs_to :user
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :collections
+  has_many :reviews, through: :collections
 end
